338571b81a95b47e4def947d2cd821e4b73e457e
jiujiuWan <1803102562@qq.com>
2022-09-20 14:01:15 +0800

    第一次提交

---
+++ src/views/HomeView.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" @click="add()">
+    <div>{{obj.name}}</div>
+    <div>{{count}}</div>
+  </div>
+</template>
+
+<script>
+import HelloWorld from '@/components/HelloWorld.vue'
+import { ref, reactive } from 'vue'
+export default {
+  name: 'HomeView',
+  setup() {
+    // let num = 0
+    let count = ref(100)
+    const add = () => {
+      // count.value++
+      obj.name = '啾啾丸天下无双'
+    }
+    let obj = reactive({
+      name: '啾啾丸',
+      age: 18
+    })
+    console.log(ref);
+    return {
+      obj,
+      count,
+      add
+    }
+  },
+  components: {
+    HelloWorld
+  }
+}
+</script>

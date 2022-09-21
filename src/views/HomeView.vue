cb6cf667721be4d50b6f1c758038e89a6dc198fe
jiujiuWan <1803102562@qq.com>
2022-09-21 17:57:35 +0800

    提交云端

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,93 +1,50 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <input type="text" v-model="jiuJiuWan" />
-    <input type="text" v-model="setAndGet" />
-    <!-- <img alt="Vue logo" src="../assets/logo.png" @click="add()"> -->
-    <!-- <div>{{obj.name}}</div>
-    <div>{{count}}</div>
-    {{a}}
-   
-    <helloChild :name="obj.name" @ziDingYi="JJJ" /> -->
+    <img alt="Vue logo" src="../assets/logo.png" @click="changeOne">
+    <!-- <div @click="goChild()">点我编程式跳转路由</div>
+    <div @click="goChild()">点我声明式跳转路由</div> -->
+    {{name}}
+    {{age}}
+    {{info.a}}
+    {{info.b}}
   </div>
 </template>
 
 <script>
-import { LOGICAL_OPERATORS } from '@babel/types'
-import { computed, reactive } from 'vue'
-// import helloChild from "../components/helloChild.vue"
+import { reactive, toRefs } from '@vue/reactivity'
+// import router from '@/router'
+//传递参数的页面
 export default {
-  setup() {
-    //计算属性数据
 
+  setup() {
     let obj = reactive({
-      Name1: "张三",
-      Age: 18
-    })
-    //只显示(单向)
-    let jiuJiuWan = computed(() => {
-      return obj.Name1 + obj.Age
-    })
-    //显示和修改(双向)
-    let setAndGet = computed({
-      get() {
-        return obj.Name1
-      },
-      set(val) {
-        // obj.Age = ''
-        obj.Name1 = val
-        // console.log(val);
+      name: '张三',
+      age: 18,
+      info: {
+        a: 'aaaa',
+        b: 'bbbb'
       }
     })
-    // //响应式数据
-    // let count = ref(100)
-    // //子组件传来的值
-    // let a = ref(0)
-    // //修改页面数据
-    // const add = () => {
-    //   obj.name = '啾啾丸天下无双(master)(a分支)'
+    function changeOne() {
+      obj.info.a += '1'
+    }
+    const { name, age } = toRefs(obj)
+    // setInterval(() => {
+    //   name.value += '啾啾丸'
+    //   // console.log(name.value);
+    // }, 1000);
+    // const router = useRouter()
+    // let nums = 123
+    // function goChild() {
+    //   router.push({ path: 'childRouter', query: { nums } })
     // }
-    // //定义一个
-    // let obj = reactive({
-    //   name: '啾啾丸',
-    //   age: 18
-    // })
-    // //子传父（接收）
-    // function JJJ(num) {
-    //   a.value = num
-    // }
-    // //生命周期函数
-    // onBeforeMount(() => {
-    //   console.log('onBeforeMount');
-    // })
-    // onMounted(() => {
-    //   console.log('onMounted');
-    // })
-    // onBeforeUpdate(() => {
-    //   console.log('onBeforeUpdate');
-    // })
-    // onUpdated(() => {
-    //   console.log('onUpdated');
-    // })
-    // onBeforeUnmount(() => {
-    //   console.log('onBeforeUnmount');
-    // })
-    // onUnmounted(() => {
-    //   console.log('onUnmounted');
-    // })
-
     return {
-      // obj,
-      // count,
-      // add,
-      // JJJ,
-      // a
-      jiuJiuWan,
-      setAndGet
+      name,
+      age,
+      ...toRefs(obj),
+      changeOne
+      // goChild,
     }
   }
-  // components: {
-  // helloChild
-  // }
 }
 </script>
---
+++ src/views/childRouter.vue
@@ -0,0 +1,22 @@
+<template>
+    <img src="../assets/img123.png" class="img">
+</template>
+
+
+<script>
+//接受参数的页面
+import { useRoute } from 'vue-router'
+export default {
+    setup() {
+        const route = useRoute()
+        console.log(route.query.nums);
+    }
+}
+</script>
+
+<style>
+.img {
+    width: 500px;
+    height: 300px;
+}
+</style>
